ec619dd7aaff0ebee15e4d54698281f2013966fe
Anne-Marie Couture <annemarie.couture@hotmail.com>
2023-05-01 10:07:35 -0400

    #Two Master Materials

---
+++ MasterMaterial.py
@@ -0,0 +1,39 @@
+import unreal
+
+AssetTools = unreal.AssetToolsHelpers.get_asset_tools()
+MaterialEditLibrary = unreal.MaterialEditingLibrary
+EditorAssetLibrary = unreal.EditorAssetLibrary
+
+Materials = AssetTools.create_asset("M_Master", "/Game/Materials", unreal.Material, unreal.MaterialFactoryNew())
+
+#Create 2D Texture Param and Connect to Base Color
+BaseColorTextureParam = MaterialEditLibrary.create_material_expression(Materials, unreal.MaterialExpressionTextureSampleParameter, -384, -300)
+BaseColorTextureParam.set_editor_property("ParameterName", "BaseColor")
+MaterialEditLibrary.connect_material_property(BaseColorTextureParam, "RGB", unreal.MaterialProperty.MP_BASE_COLOR)
+
+#Create 2D Texture Param and Connect to Roughness
+RoughnessTextureParam = MaterialEditLibrary.create_material_expression(Materials, unreal.MaterialExpressionTextureSampleParameter, -384, 400)
+RoughnessTextureParam.set_editor_property("ParameterName", "Roughness")
+MaterialEditLibrary.connect_material_property(RoughnessTextureParam, "RGB", unreal.MaterialProperty.MP_ROUGHNESS)
+
+#Create Constant Value and Connect to Specular
+SpecularParam = MaterialEditLibrary.create_material_expression(Materials, unreal.MaterialExpressionConstant, -384, 300)
+SpecularParam.set_editor_property("R", 0.3)
+MaterialEditLibrary.connect_material_property(SpecularParam, "", unreal.MaterialProperty.MP_SPECULAR)
+
+#Create 2D Texture Param and Connect to Normal
+NormalTextureParam = MaterialEditLibrary.create_material_expression(Materials, unreal.MaterialExpressionTextureSampleParameter, -384, 700)
+NormalTextureParam.set_editor_property("ParameterName", "Normal")
+MaterialEditLibrary.connect_material_property(NormalTextureParam, "RGB", unreal.MaterialProperty.MP_NORMAL)
+
+#Create 2D Texture Param and Connect to Metallic
+MetallicTextureParam = MaterialEditLibrary.create_material_expression(Materials, unreal.MaterialExpressionTextureSampleParameter, -384, 0)
+MetallicTextureParam.set_editor_property("ParameterName", "Metallic")
+MaterialEditLibrary.connect_material_property(MetallicTextureParam, "RGB", unreal.MaterialProperty.MP_METALLIC)
+
+#Create 2D Texture Param and Connect to AO
+AOTextureParam = MaterialEditLibrary.create_material_expression(Materials, unreal.MaterialExpressionTextureSampleParameter, -384, 1000)
+AOTextureParam.set_editor_property("ParameterName", "AO")
+MaterialEditLibrary.connect_material_property(AOTextureParam, "RGB", unreal.MaterialProperty.MP_AMBIENT_OCCLUSION)
+
+EditorAssetLibrary.save_asset("/Game/Materials/M_Master", True)
---
+++ MasterMaterialStacked.py
@@ -0,0 +1,38 @@
+import unreal
+
+AssetTools = unreal.AssetToolsHelpers.get_asset_tools()
+MaterialEditLibrary = unreal.MaterialEditingLibrary
+EditorAssetLibrary = unreal.EditorAssetLibrary
+
+Materials = AssetTools.create_asset("M_Master_Stacked", "/Game/Materials", unreal.Material, unreal.MaterialFactoryNew())
+
+#Create 2D Texture Param and Connect to Base Color
+BaseColorTextureParam = MaterialEditLibrary.create_material_expression(Materials, unreal.MaterialExpressionTextureSampleParameter, -384, -200)
+BaseColorTextureParam.set_editor_property("ParameterName", "BaseColor")
+MaterialEditLibrary.connect_material_property(BaseColorTextureParam, "RGB", unreal.MaterialProperty.MP_BASE_COLOR)
+
+#Create Constant Value and Connect to Specular
+SpecularParam = MaterialEditLibrary.create_material_expression(Materials, unreal.MaterialExpressionConstant, -384, 100)
+SpecularParam.set_editor_property("R", 0.3)
+MaterialEditLibrary.connect_material_property(SpecularParam, "", unreal.MaterialProperty.MP_SPECULAR)
+
+#Create 2D Texture Param and Connect to Normal
+NormalTextureParam = MaterialEditLibrary.create_material_expression(Materials, unreal.MaterialExpressionTextureSampleParameter, -384, 200)
+NormalTextureParam.set_editor_property("ParameterName", "Normal")
+MaterialEditLibrary.connect_material_property(NormalTextureParam, "RGB", unreal.MaterialProperty.MP_NORMAL)
+
+#Create 2D Texture Param and Connect to ORM
+ORMTextureParam = MaterialEditLibrary.create_material_expression(Materials, unreal.MaterialExpressionTextureSampleParameter, -384, 500)
+ORMTextureParam.set_editor_property("ParameterName", "ORM")
+MaterialEditLibrary.connect_material_property(ORMTextureParam, "R", unreal.MaterialProperty.MP_AMBIENT_OCCLUSION)
+MaterialEditLibrary.connect_material_property(ORMTextureParam, "G", unreal.MaterialProperty.MP_ROUGHNESS)
+MaterialEditLibrary.connect_material_property(ORMTextureParam, "B", unreal.MaterialProperty.MP_METALLIC)
+
+#Create Material Instance
+MI_Stacked = AssetTools.create_asset("MI_Stacked", "/Game/Materials", unreal.MaterialInstanceConstant, unreal.MaterialInstanceConstantFactoryNew())
+#MI_Stacked.set_editor_property("Parent", "M_Master_Stacked")
+
+#Save Materials and Instances
+EditorAssetLibrary.save_asset("/Game/Materials/M_Master_Stacked", True)
+EditorAssetLibrary.save_asset("/Game/Materials/MI_Stacked", True)
+
